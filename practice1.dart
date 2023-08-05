e9cf7e5de98f1d76cba62a183daef178aad9cc8b
Saniath Rahman Khan <74543953+SaniathItachi@users.noreply.github.com>
2023-08-05 11:30:47 +1000

    Add files via upload

---
+++ practice1.dart
@@ -0,0 +1,17 @@
+void main(){
+  const a = 40;
+  const b = 20;
+
+  const add = a + b;
+  const sub = a-b;
+  const mul = a*b;
+  const div = a/b;
+  const mod = a % b;
+  print("Addition : " + "40+20 = " + (add).toString());
+  print("Addition : " + "40-20 = " + (sub).toString());
+  print("Multiplication : " + "40*20 = " + (mul).toString());
+  print("Division : " + "40/20 = " + (div).toString());
+  print("Modulus : " + "40%20 = " + (mod).toString());
+
+
+}
